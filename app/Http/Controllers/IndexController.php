e9faee46719fab1687bcc36683dcbb36370dc9fc
Andrey <gorand.one@gmail.com>
2019-06-09 14:01:34 +0300

    add menu

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Page;
+use App\People;
+use App\Portfolio;
+use App\Service;
+
+
 
 class IndexController extends Controller
 {
-    public function execute()
+    public function execute(Request $request)
     {
-        return view('layouts.site');
+    	$pages = Page::all();
+    	$peoples = People::all();
+    	$portfolio = Portfolio::get();
+    	$services = Service::get();
+    	
+    	$menu = array();
+    	foreach ($pages as $page) {
+    		$item = array('title'=>$page->name, 'alias'=>$page->alias);
+    		array_push($menu, $item);
+    		
+    	}
+
+    	$item = array('title'=>'Services', 'alias'=>'service');
+    	array_push($menu, $item);
+
+    	$item = array('title'=>'Portfolio', 'alias'=>'portfolio');
+    	array_push($menu, $item);
+
+    	
+
+    	$item = array('title'=>'Team', 'alias'=>'team');
+    	array_push($menu, $item);
+
+    	$item = array('title'=>'Contact', 'alias'=>'contact');
+    	array_push($menu, $item);
+
+    	// dd($menu);
+        return view('site.index', array(
+        	'menu'=>$menu, 'portfolio'=>$portfolio, 'services'=>$services, 'peoples'=>$peoples));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,75 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-//*****Page*****//
-
-Route::group(['middleware'=>'web'], function(){
-	Route::match(['get', 'post'], '/', 'IndexController@execute')->name('home');
-	// Route::get('/page/{alias}', ['uses'=>'PageController@execute', 'as'=>'page']);
-
+Route::group(['middleware'=>'web'],function() {
+	
+	Route::match(['get','post'],'/',['uses'=>'IndexController@execute','as'=>'home']);
+	Route::get('/page/{alias}',['uses'=>'PageController@execute','as'=>'page']);
+	
 	Route::auth();
+	
 });
 
-
-//*****Admin*****//
-
-// Route::group(['middleware'=>'auth', 'prefix'=>'admin'], function(){
+//admin/service
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function() {
 	
-// });
-Auth::routes();
+	//admin
+	Route::get('/',function() {
+		
+		
+		
+	});
+	
+	//admin/pages
+	Route::group(['prefix'=>'pages'],function() {
+		
+		///admin/pages
+		Route::get('/',['uses'=>'PagesController@execute','as'=>'pages']);
+		
+		//admin/pages/add
+		Route::match(['get','post'],'/add',['uses'=>'PagesAddController@execute','as'=>'pagesAdd']);
+		//admin/edit/2
+		Route::match(['get','post','delete'],'/edit/{page}',['uses'=>'PagesEditController@execute','as'=>'pagesEdit']);
+		
+	});
+	
+	
+	Route::group(['prefix'=>'portfolios'],function() {
+		
+		
+		Route::get('/',['uses'=>'PortfolioController@execute','as'=>'portfolio']);
+		
+		
+		Route::match(['get','post'],'/add',['uses'=>'PortfolioAddController@execute','as'=>'portfolioAdd']);
+		
+		Route::match(['get','post','delete'],'/edit/{portfolio}',['uses'=>'PortfolioEditController@execute','as'=>'portfolioEdit']);
+		
+	});
+	
+	
+	Route::group(['prefix'=>'services'],function() {
+		
+		
+		Route::get('/',['uses'=>'ServiceController@execute','as'=>'services']);
+		
+		
+		Route::match(['get','post'],'/add',['uses'=>'ServiceAddController@execute','as'=>'serviceAdd']);
+		
+		Route::match(['get','post','delete'],'/edit/{service}',['uses'=>'ServiceEditController@execute','as'=>'serviceEdit']);
+		
+	});
+	
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
